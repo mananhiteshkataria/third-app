df155f228ae7fbfe8095571cf4bab6b99192dad8
mananhiteshkataria <47041531+mananhiteshkataria@users.noreply.github.com>
2023-01-18 17:16:35 +0530

    added service to display data

--- src/app/fake/fake.component.ts
+++ src/app/fake/fake.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-fake',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./fake.component.css']
 })
 export class FakeComponent {
+constructor(public service:UserService) 
+{}
+  users : any=[];
+  handleClick(){
+    //only on subscribe will data be available
+    //subscribe takes object with properties like next and error
+    this.service.getFakeJson().subscribe({
+      next:(value)=>this.users=value,
+      error:(value)=> console.log(value)
+    });
+  }
+  
 
 }
