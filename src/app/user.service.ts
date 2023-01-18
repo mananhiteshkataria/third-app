b54c85ca1f945b481d8b7ae258ce07588506501e
mananhiteshkataria <47041531+mananhiteshkataria@users.noreply.github.com>
2023-01-18 17:01:46 +0530

    creating service and fetching values

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getFakeJson(): Observable<any>{
+    let url="https://jsonplaceholder.typicode.com/users";
+    return this.http.get(url);
+  }
 }
